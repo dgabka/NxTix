e8fcc31350c039969c59d8f7e0f1de99c4253208
Dawid Gąbka <gabka.daw@gmail.com>
2022-11-07 15:39:27 +0100

    create shared eslint and tsconfig settings

---
+++ packages/eslint-config-nxtix/index.js
@@ -0,0 +1,17 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    sourceType: 'module',
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'turbo',
+    'prettier',
+  ],
+  rules: {
+    'prefer-const': 'error',
+    '@typescript-eslint/no-unused-vars': 'error',
+  },
+};
